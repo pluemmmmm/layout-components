4213370c52b91c10e5e1fb5a908ebe0780384857
nongdforwork <nongd@gmail.com>
2023-06-07 17:54:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from './Box';
+import Grid from './Grid';
+import Notification from './Notification';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='app-section'>
+      <div className='app-container'>
+        <h2>One Piece</h2>
+        <Notification title="สมบัติของชั้นน่ะหรอ" description="อยากได้ก็ไปเอาสิ ไปหาเอาเลย" />
+        <Grid columns={3}>
+          <Box title='วันพีซน่ะมีอยู่จริง'>
+            <ul>
+              <li>ชั้นมาเพื่อหยุดสงครามนี้</li>
+              <li>ชั้นจะเป็นราชาโจรสลัดให้ได้เล้ย</li>
+              <li>บาดแผลที่กลางหลังคือความอับอายของนักดาบ</li>
+            </ul>
+          </Box>
+          <Box title='ตัวละคร'>
+            <p>หมายเหตุ : คำพูดของตัวละคร</p>
+            <ol>
+              <li li>แชงคูส</li>
+              <li>ลูฟี่</li>
+              <li>โซโล</li>
+            </ol>
+          </Box>
+          <Box title='ฉายา'>
+            <p>ผมแดง</p>
+            <p>หมวกฟาง</p>
+            <p>นักล่าค่าหัว</p>
+          </Box>
+        </Grid>
+      </div>
+    </section>
   );
 }
 
---
+++ src/Grid.js
@@ -0,0 +1,15 @@
+import './Grid.css';
+
+function Grid(props) {
+    const { columns, children } = props;
+
+    let gridClass = `grid grid-${columns}`;
+
+    return (
+        <div className={gridClass}>
+            {children}
+        </div>
+    );
+}
+
+export default Grid;
